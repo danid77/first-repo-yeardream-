61ed8d1f80eb3b54d52a349d967c356617bf98d4
danid77 <jin.danid@gmail.com>
2022-05-06 13:39:41 +0900

    과제1 수정 및 과제2 작성

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
 print('hello world')
 for i in range(10):
-    if i == 3:
+    if i % 3 == 0:
         print("year")
     else:
         print(i)
+
+
+for j in range(20):
+    if j % 5 == 0:
+        if j % 15 == 0:
+            print("yeardream")
+        else:
+            print("dream")
+    else:
+        print(j)
+
